a69cbc8c37a0c43a82b7f69d47fad6afcf805f12
Tatiana-Chaykina <t.e.chaykina@gmail.com>
2024-12-20 20:36:06 +0700

    Add files via upload

---
+++ 5 chapter/🐟.py
@@ -0,0 +1,14 @@
+sea_fish        = ["shark", "flounder", "tuna", "cod", "herring", "Marlin"]
+freshwater_fish = ["Asp", "Pike", "Carp", "Salmon", "Ide", "Trout"]
+
+a=[sea_fish+freshwater_fish]
+a=(sorted(([i.capitalize() for i in sea_fish ]+[i.capitalize() for i in freshwater_fish])))
+
+for i in a:
+    for i1 in sea_fish:
+        if i==i1.capitalize():
+            a[a.index(i)]=i1
+    for i1 in freshwater_fish:
+        if i==i1.capitalize():
+            a[a.index(i)]=i1
+print(a)
